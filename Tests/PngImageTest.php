d325e5db0620561dd67e7a3fdc4061fd85d25413
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-11-08 17:24:21 +0100

    Improve PngImageTest

--- Tests/PngImageTest.php
+++ Tests/PngImageTest.php
@@ -2,24 +2,52 @@
 
 namespace HeatMap\Tests;
 
+use HeatMap\Color;
 use HeatMap\PngImage;
 
 class PngImageTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  PngImage */
+    private $pngImage;
 
-    public function setUp(){}
+    public function setUp(){
+        $this->pngImage = new PngImage(100, 100);
+    }
 
     public function testCanInstantiate(){
-        $this->assertInstanceOf(PngImage::class, new PngImage(100,100));
+        $this->assertInstanceOf(PngImage::class, $this->pngImage);
+    }
+
+    public function testSetPixelWithXLowerThanZeroThrowsException(){
+        $this->expectException(\InvalidArgumentException::class);
+        $this->pngImage->setPixel(-1, 0, Color::fromHex('#123456'));
+    }
+
+    public function testSetPixelWithXHigherThanImageWidthThrowsException(){
+        $this->expectException(\InvalidArgumentException::class);
+        $this->pngImage->setPixel(101, 0, Color::fromHex('#123456'));
+    }
+
+    public function testSetPixelWithYLowerThanZeroThrowsException(){
+        $this->expectException(\InvalidArgumentException::class);
+        $this->pngImage->setPixel(0, -1, Color::fromHex('#123456'));
+    }
+
+    public function testSetPixelWithYHigherThanImageWidthThrowsException(){
+        $this->expectException(\InvalidArgumentException::class);
+        $this->pngImage->setPixel(0, 101, Color::fromHex('#123456'));
     }
 
     public function testCanSaveImageFile(){
         $fileName = __DIR__.'/test.png';
-
-        $pngImage = new PngImage(100, 100);
-        $pngImage->save($fileName);
+        $this->pngImage->save($fileName);
         $this->assertTrue(file_exists($fileName));
         unlink($fileName);
         $this->assertFalse(file_exists($fileName));
+    }
+
+    public function tearDown(){
+        $this->pngImage->destroy();
+        unset($this->pngImage);
     }
 }
